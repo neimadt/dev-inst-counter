20c4818077d0da69f8db2633b32929d0f8332f67
neimadt <damtoulet@yahoo.fr>
2023-07-29 10:31:10 +0400

    added host

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ const PORT = process.env.PORT || 5174;
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: PORT
+    port: PORT,
+    host: '0.0.0.0'
   },
   preview: {
-    port: PORT
+    port: PORT,
+    host: '0.0.0.0'
   },
   plugins: [react()],
 })
