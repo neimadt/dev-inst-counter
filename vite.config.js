d32cf88bdb1d4540f2cfc816bb894d48d4f220d1
neimadt <damtoulet@yahoo.fr>
2023-07-29 10:24:30 +0400

    added port

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const PORT = process.env.PORT || 5174;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: PORT
+  },
+  preview: {
+    port: PORT
+  },
   plugins: [react()],
 })
